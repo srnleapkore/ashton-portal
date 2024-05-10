53501eb972495d6594781f6f9969d2749404d6b8
srnleapkore <sarant.srn@gmail.com>
2024-05-10 12:46:53 +0530

    Created Pages for sales, login, signup, and setup done with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SalesDashboard from "./SalesTeam/SalesDashboard/SalesDashboard";
+import Login from "./AuthPage/LoginPage/Login";
+import Signup from "./AuthPage/SignupPage/Signup";
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sales-portal" element={<SalesDashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ frontend/src/SalesTeam/SalesDashboard/SalesDashboard.jsx
@@ -0,0 +1,5 @@
+export default function SalesDashboard() {
+  return (
+    <div>SalesDashboard</div>
+  )
+}
